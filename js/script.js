9423698442c3d2ba38debfa4d651ddf34984a4fd
Carlos <carlos_valencia@live.com>
2016-06-22 21:10:12 -0500

    mobile version added

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 
 		var $sliderUl = $('.testimonies-list');
 		var $slides = $sliderUl.find('li'); 
-		console.log($slides.length);
-		var width = 400;
+		var width = $('.testimonies-list li:first').width();
 		var currentSlide = 1;
 
 		function sliderSlide(){
@@ -12,9 +11,13 @@ $(document).ready(function(){
 				$sliderUl.css('margin-left',0);
 			}
 			currentSlide++;
-			$sliderUl.animate({'margin-left': '-=' + width +''}, 1000);
+			$sliderUl.animate({'margin-left': '-=' + width +''}, 800);
 		}
 
-		var sliderInterval = setInterval(sliderSlide, 3000);
+		var sliderInterval = setInterval(sliderSlide, 4000);
+
+		$('#slideMenuButton').click(function(){
+			$('#navList').toggleClass("show-menu");
+		});
 
 });
